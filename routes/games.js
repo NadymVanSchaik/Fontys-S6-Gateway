7de6a2b9fbc0f6e8398d027dda1fbe9f6fae81f3
NadymVanSchaik <34303676+NadymVanSchaik@users.noreply.github.com>
2022-06-14 14:58:22 +0200

    Added some other configurations to get RabbitMQ to work

--- routes/games.js
+++ routes/games.js
@@ -8,39 +8,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var jsonParser = bodyParser.json();
 
 
-router.get('/all', (req, res) => {
-  sendMessage("getAllMessages", "getAllMessages")
+router.get('/all', async (req, res) => {
+  console.log("Starting function")
+  const ret = await sendMessage("getAllGames", {value: "getAllGames"});
+  console.log("THIS IS WHAT IS RETURNED BITCH ", ret)
+  console.log(Date.now());
+  res.json(ret)
 });
 
 var amqplib = require('amqplib');
-var amqp = require('amqplib/callback_api')
-const amqpUrl = process.env.AMQP_URL || 'amqp://localhost:5673';
+const amqpUrl = process.env.AMQP_URL;
 
 
 async function sendMessage(queueName, msg) {
   const connection = await amqplib.connect(amqpUrl);
   const channel = await connection.createChannel();
-  const queue = await channel.assertQueue('', {exclusive: true});
+  const queue = await channel.assertQueue('', {exclusive: true, durable: false});
   const uuid = generateUuid();
+
+  msg = JSON.stringify({
+    value: msg.value,
+    replyTo: queue.queue,
+    id: "server-command"
+  });
 
   //Send first message
   channel.sendToQueue(queueName, Buffer.from(msg)), {
     replyTo: queue.queue,
     correlationId: uuid,
   }
-  console.log('Sent:' , msg)
 
   //Consume response
+  //TODO  This has to be resolved before return to call is given
   channel.consume(queue.queue, msg => {
-    if(msg.properties.correlationId === uuid){
+    console.log(msg.content.toString())
+    if(JSON.parse(msg.content.toString()).id === "games-response"){
       //TODO Do something with the msg
-      
-      setTimeout(() => {
-        connection.close();
-        process.exit(0);
-      }, 500)
-
-      return msg
+      console.log("Got message: ", msg)
+      console.log(Date.now());
+      connection.close();
+      return JSON.parse(msg.content.toString()).value
     } 
   }, {noAck: true});
 }
